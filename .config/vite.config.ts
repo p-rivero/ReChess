1a15340415d08ef546aff669275956fe0f60d138
privero <65060696+p-rivero@users.noreply.github.com>
2023-02-02 14:15:44 +0100

    Enable multithreading also in dev server

--- .config/vite.config.ts
+++ .config/vite.config.ts
@@ -5,7 +5,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          // These headers are required for SharedArrayBuffer to work (which is required for WASM threads)
+          // This only enables the headers for the vite dev server, make sure to add them to your production server as well
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
