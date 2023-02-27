e39ef36aedb47de7a2ca08c6e6a8409b764373cc
privero <65060696+p-rivero@users.noreply.github.com>
2023-02-27 10:48:21 +0100

    Move pages from view/ directory to pages/ directory

--- src/pages/EditPiecePage.vue
+++ src/pages/EditPiecePage.vue
@@ -5,9 +5,236 @@
 -->
 
 <template>
-  <EditPieceView/>
+  <div class="columns is-desktop reverse-columns">
+    <div class="column is-narrow left-column">
+      
+      <div class="board-container">
+        <PieceViewer :size="500" :piece="piece"/>
+      </div>
+      
+      <div class="field">
+        <label class="label">Add or remove:</label>
+        <div class="field is-grouped is-grouped-multiline">
+          <div class="control">
+            <button class="button">Move jump</button>
+          </div>
+          <div class="control">
+            <button class="button">Capture jump</button>
+          </div>
+          <div class="control">
+            <button class="button">Explosion</button>
+          </div>
+        </div>
+      </div>
+      
+      <br>
+      <button class="button is-primary bottom-button" @click="$router.push('/edit')">Done</button>
+    </div>
+    
+    
+    
+    <div class="column">
+      <input class="input is-large" type="text" placeholder="Piece name">
+      <br>
+      <br>
+      <div class="columns">
+        <div class="column">
+          <div class="horizontal-field">
+            <label class="checkbox piece-color-checkbox" style="margin-right: 1rem">
+              <input type="checkbox" v-model="whiteEnabled">
+              White
+            </label>
+            <div class="piece-image" :class="{ invisible: !whiteEnabled }"></div>
+            <EditButton :class="{ invisible: !whiteEnabled }" />
+            <input class="input width-3rem" :class="{ invisible: !whiteEnabled }" type="text" placeholder="A">
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="horizontal-field">
+            <label class="checkbox" style="margin-right: 1rem">
+              <input type="checkbox" v-model="blackEnabled">
+              Black
+            </label>
+            <div class="piece-image" :class="{ invisible: !blackEnabled }"></div>
+            <EditButton :class="{ invisible: !blackEnabled }" />
+            <input class="input width-3rem" :class="{ invisible: !blackEnabled }" type="text" placeholder="a">
+          </div>
+        </div>
+      </div>
+      
+      <label class="label">Behavior:</label>
+      <SmartCheckbox text="Leader" class="rules-field"
+        :startValue="piece?.isLeader"
+        :onChanged="value => { piece!.isLeader = value; draftStore.save() }"/>
+      
+      <div class="horizontal-field">
+        <div class="field-label">
+          <label>Castling</label>
+        </div>
+        <div class="select">
+          <select>
+            <option>No</option>
+            <option>As king</option>
+            <option>As rook</option>
+          </select>
+        </div>
+        <div class="field-label-both">
+          <label>Queenside file</label>
+        </div>
+        <input class="input width-3rem" type="text" placeholder="c">
+        <div class="field-label-both">
+          <label>Kingside file</label>
+        </div>
+        <input class="input width-3rem" type="text" placeholder="g">
+      </div>
+      
+      <div class="columns">
+        <div class="column ">
+          <SmartCheckbox text="Explode when capturing" class="rules-field"
+            :startValue="piece?.explodes"
+            :onChanged="value => { piece!.explodes = value; draftStore.save() }"/>
+        </div>
+        <div class="column">
+          <SmartCheckbox text="Immune to other explosions" class="rules-field"
+            :startValue="piece?.immuneToExplosion"
+            :onChanged="value => { piece!.immuneToExplosion = value; draftStore.save() }"/>
+        </div>
+      </div>
+      <br>
+      
+      <label class="label">Movement:</label>
+      <div style="margin-bottom: 1rem;"> <MovementSlideRow :piece-index="pieceIndex" :type="'move'"/> </div>
+      <label>Double jump when standing on:</label>
+      <CoordPillList :editable="true" style="margin-bottom: 1.5rem;"
+        :starting-coords="piece?.doubleJumpSquares"
+        :on-changed="coords => {piece!.doubleJumpSquares = coords; draftStore.save()}"/>
+        
+      <label class="label">Capture:</label>
+      <div style="margin-bottom: 1.5rem;"> <MovementSlideRow :piece-index="pieceIndex" :type="'capture'"/> </div>
+      
+      <label class="label">Promotion:</label>
+      <label>Promote when landing on:</label>
+      <CoordPillList :editable="true"
+        :starting-coords="piece?.promotionSquares"
+        :on-changed="coords => {piece!.promotionSquares = coords; draftStore.save()}"/>
+      <br>
+        
+      <div class="columns">
+        <div class="column" :class="{ invisible: !whiteEnabled }">
+          <label>(White) Promote to:</label>
+          <CharPillList :editable="true"
+            :starting-pills="piece?.promoVals[0]"
+            :on-changed="promos => {piece!.promoVals[0] = promos; draftStore.save()}"/>
+        </div>
+        
+        <div class="column" :class="{ invisible: !blackEnabled }">
+          <label>(Black) Promote to:</label>
+          <CharPillList :editable="true"
+            :starting-pills="piece?.promoVals[1]"
+            :on-changed="promos => {piece!.promoVals[1] = promos; draftStore.save()}"/>
+        </div>
+      </div>
+      
+    </div>
+  </div>
 </template>
 
+
 <script setup lang="ts">
-  import EditPieceView from './view/EditPieceView.vue'
+  import PieceViewer from '@/components/ChessBoard/PieceViewer.vue'
+  import EditButton from '@/components/EditButton.vue'
+  import MovementSlideRow from '@/components/EditVariant/MovementSlideRow.vue'
+  import SmartCheckbox from '@/components/BasicWrappers/SmartCheckbox.vue'
+  import CharPillList from '@/components/EditVariant/CharPillList.vue'
+  import CoordPillList from '@/components/EditVariant/CoordPillList.vue'
+  import type { PieceDefinition } from '@/protochess/interfaces'
+  import { router } from '@/router'
+  import { useVariantDraftStore } from '@/stores/variant-draft'
+  import { paramToInt } from '@/utils/param-to-int'
+  import { ref } from 'vue'
+  import { useRoute } from 'vue-router'
+  
+  const whiteEnabled = ref(true)
+  const blackEnabled = ref(true)
+  
+  const route = useRoute()
+  const draftStore = useVariantDraftStore()
+  const pieceIndex = paramToInt(route.params.pieceIndex)
+  let piece: PieceDefinition|null = null
+  if (Number.isNaN(pieceIndex) || pieceIndex < 0 || pieceIndex >= draftStore.state.pieceTypes.length) {
+    // Incorrect piece index, redirect to edit-variant
+    router.push({ name: 'edit-variant' })
+  } else {
+    piece = draftStore.state.pieceTypes[pieceIndex]
+  }
 </script>
+
+
+<style lang="scss" scoped>
+  @media screen and (max-width: 1023px) {
+    .reverse-columns {
+      display: flex;
+      flex-direction: column-reverse;
+    }
+  }
+  
+  .left-column {
+    max-width: 500px;
+    margin-right: 2rem;
+  }
+  
+  .field-label {
+    flex-shrink: 0;
+    flex-grow: 0;
+    align-items: center;
+    margin-right: 1rem;
+  }
+  .field-label-both {
+    @extend .field-label;
+    margin-left: 1rem;
+  }
+  .field-label-right {
+    @extend .field-label-both;
+    margin-right: 0;
+  }
+  
+  .horizontal-field {
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+  }
+  
+  .board-container {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+  
+  .bottom-button {
+    width: 45%;
+    margin-bottom: 0.5rem;
+    margin-right: 0.5rem;
+  }
+  
+  .rules-field:not(:last-child) {
+    margin-bottom: 1rem;
+    flex-basis: content;
+  }
+  
+  .width-3rem {
+    width: 3rem;
+  }
+  
+  .piece-image {
+    width: 4rem;
+    height: 4rem;
+    flex-shrink: 0;
+    background-image: url("@/assets/img/pieces/knook.svg");
+    background-size: contain;
+  }
+  
+  .invisible {
+    visibility: hidden;
+  }
+</style>
--- src/pages/EditVariantPage.vue
+++ src/pages/EditVariantPage.vue
@@ -1,13 +1,203 @@
-
 <!-- 
   This page is used to edit the variant that is currently stored in LocalStorage (through the use of the draftVariantStore)  
   
 -->
 
 <template>
-  <CreateVariantView/>
+  <div class="columns is-desktop reverse-columns">
+    <div class="column is-narrow left-column">
+      
+      <div class="board-container">
+        <ViewableChessBoard ref="board" :size="500" :white-pov="true" :view-only="false" :show-coordinates="true"
+          :key="stateKey" :after-mount="updateBoard"/>
+      </div>
+      
+      <div class="horizontal-field">
+        <div class="field-label"><label>Board size:</label></div>
+        <SmartNumberInput class="width-5rem" :min="2" :max="16" :default="8"
+          :start-value="draftStore.state.boardHeight"
+          :on-changed="height => { draftStore.state.boardHeight = height; draftStore.save() }"/>
+        <div class="field-label-both"><label>x</label></div>
+        <SmartNumberInput class="width-5rem" :min="2" :max="16" :default="8"
+          :start-value="draftStore.state.boardWidth"
+          :on-changed="width => { draftStore.state.boardWidth = width; draftStore.save() }"/>
+      </div>
+      
+      <div class="horizontal-field">
+        <div class="field-label">
+          <label>First player to move:</label>
+        </div>
+        <div class="field-body">
+          <SmartDropdown :items="['White', 'Black']"
+            :start-item="draftStore.state.playerToMove === 0 ? 'White' : 'Black'"
+            :on-changed="item => { draftStore.state.playerToMove = item === 'White' ? 0 : 1; draftStore.save() }"/>
+        </div>
+      </div>
+      
+      <div class="field">
+        <label class="label">Place piece:</label>
+        <PiecePlacementButtons :state="draftStore.state" :key="stateKey"/>
+      </div>
+      
+      <br>
+      <br>
+      <button class="button bottom-button">Analysis board</button>
+      <button class="button bottom-button">Play against engine</button>
+      <br>
+      <button class="button is-primary bottom-button">Publish</button>
+      <br>
+      * Your draft is saved automatically, you can close this page and come back later to continue editing.
+      
+    </div>
+    
+    
+    
+    <div class="column">
+      <input class="input is-large" type="text" placeholder="Variant name" ref="variantNameInput">
+      <br>
+      <br>
+      <textarea class="textarea" placeholder="Describe the rules of the variant and how fun it is to play!" ref="descriptionInput"></textarea>
+      <br>
+      
+      <label class="label">Rules:</label>
+      <div class="columns is-mobile">
+        <div class="column is-narrow left-column">
+          <SmartCheckbox text="Capturing is forced" class="rules-field"
+            :start-value="draftStore.state.globalRules.capturingIsForced"
+            :on-changed="value => { draftStore.state.globalRules.capturingIsForced = value; draftStore.save() }"/>
+          <br>
+          <SmartCheckbox text="Check is forbidden" class="rules-field"
+            :start-value="draftStore.state.globalRules.checkIsForbidden"
+            :on-changed="value => { draftStore.state.globalRules.checkIsForbidden = value; draftStore.save() }"/>
+        </div>
+        
+        <div class="column">
+          <SmartCheckbox text="Stalemated player loses" class="rules-field"
+            :start-value="draftStore.state.globalRules.stalematedPlayerLoses"
+            :on-changed="value => { draftStore.state.globalRules.stalematedPlayerLoses = value; draftStore.save() }"/>
+          <br>
+          <SmartCheckbox text="Invert ALL win conditions" class="rules-field"
+            :start-value="draftStore.state.globalRules.invertWinConditions"
+            :on-changed="value => { draftStore.state.globalRules.invertWinConditions = value; draftStore.save() }"/>
+        </div>
+      </div>
+      <div class="horizontal-field">
+        <div class="field-label">
+          <label>Repetitions for draw:</label>
+        </div>
+        <SmartNumberInput class="width-5rem" :min="0" :max="200" :default="3"
+          :start-value="draftStore.state.globalRules.repetitionsDraw"
+          :on-changed="value => { draftStore.state.globalRules.repetitionsDraw = value; draftStore.save() }"/>
+      </div>
+      <div class="horizontal-field">
+        <div class="field-label">
+          <label>Lose when put in check</label>
+        </div>
+        <SmartNumberInput class="width-5rem" :min="0" :max="200" :default="0" placeholder="-"
+          :start-value="draftStore.state.globalRules.checksToLose"
+          :on-changed="value => { draftStore.state.globalRules.checksToLose = value; draftStore.save() }"/>
+        
+        <div class="field-label-right">
+          <label>times</label>
+        </div>
+      </div>
+      <br>
+      <label class="label">Pieces:</label>
+      <PiecesSummary 
+        :editable="true"
+        :state="draftStore.state"
+        :on-edit-click="pieceIndex => $router.push({ name: 'edit-piece', params: { pieceIndex } })"
+        :on-delete-click="pieceIndex => deletePiece(pieceIndex)" />
+      
+    </div>
+  </div>
 </template>
 
+
 <script setup lang="ts">
-  import CreateVariantView from './view/EditVariantView.vue'
+  import ViewableChessBoard from '@/components/ChessBoard/ViewableChessBoard.vue'
+  import PiecesSummary from '@/components/EditVariant/PiecesSummary.vue'
+  import SmartCheckbox from '@/components/BasicWrappers/SmartCheckbox.vue'
+  import SmartNumberInput from '@/components/BasicWrappers/SmartNumberInput.vue'
+  import SmartDropdown from '@/components/BasicWrappers/SmartDropdown.vue'
+  import { ref, computed } from 'vue'
+  import { useVariantDraftStore } from '@/stores/variant-draft'
+  import { getProtochess } from '@/protochess/protochess'
+  import PiecePlacementButtons from '@/components/EditVariant/PiecePlacementButtons.vue'
+  
+  const draftStore = useVariantDraftStore()
+  const board = ref<InstanceType<typeof ViewableChessBoard>>()  
+  const stateKey = computed(() => JSON.stringify(draftStore.state))
+  
+  async function updateBoard() {
+    if (board.value === undefined) {
+      throw new Error('Reference to board is undefined')
+    }
+    // TODO: Remove this and instead put the fen and inCheck (optional) in the GameState
+    const protochess = await getProtochess()
+    await protochess.setState(draftStore.state)
+    board.value.setState(await protochess.getState())
+  }
+  
+  function deletePiece(pieceIndex: number) {
+    // TODO: Ask for confirmation
+    draftStore.state.pieces.splice(pieceIndex, 1)
+    draftStore.save()
+  }
 </script>
+
+
+<style lang="scss" scoped>
+  @media screen and (max-width: 1023px) {
+    .reverse-columns {
+      display: flex;
+      flex-direction: column-reverse;
+    }
+  }
+  
+  .left-column {
+    max-width: 500px;
+    margin-right: 2rem;
+  }
+  
+  .field-label {
+    flex-basis: auto;
+    flex-grow: 0;
+    margin-right: 1rem;
+  }
+  .field-label-both {
+    @extend .field-label;
+    margin-left: 1rem;
+  }
+  .field-label-right {
+    @extend .field-label-both;
+    margin-right: 0;
+  }
+  
+  .horizontal-field {
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+  }
+  
+  .board-container {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+    height: 500px;
+  }
+  
+  .bottom-button {
+    width: 45%;
+    margin-bottom: 0.5rem;
+    margin-right: 0.5rem;
+  }
+  
+  .rules-field:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+  
+  .width-5rem {
+    width: 5rem;
+  }
+</style>
