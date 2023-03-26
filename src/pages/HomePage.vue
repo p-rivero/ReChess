e4ff6781a48d7e8b1de61e463ccae87f54396faf
privero <65060696+p-rivero@users.noreply.github.com>
2023-03-26 15:57:25 +0200

    Remove VariantList component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -14,18 +14,60 @@
         </p>
       </div>
     </div>
-    <VariantList />
+    
+    <div class="field is-grouped is-grouped-multiline is-align-items-center is-justify-content-center">
+      <DraftCard v-if="showDraftCard" />
+      <VariantCard
+        v-for="(variant, index) of variantStore.variantList"
+        :key="index"
+        :variant="variant"
+        @play-clicked="playClicked(variant)"
+        @edit-variant="$router.push({name: 'edit-variant'})"
+      />
+      <PlayPopup ref="playPopup" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import VariantList from '@/components/Variant/View/VariantList.vue'
+  import { ref, computed, onMounted } from 'vue'
+  import { useVariantStore } from '@/stores/variant'
+  import { useAuthStore } from '@/stores/auth-user'
   import SmartTextInput from '@/components/BasicWrappers/SmartTextInput.vue'
-  import { onMounted } from 'vue'
+  import VariantCard from '@/components/Variant/View/VariantCard.vue'
+  import DraftCard from '@/components/Variant/View/DraftCard.vue'
+  import PlayPopup from '@/components/GameUI/PlayPopup.vue'
+  import { showPopup } from '@/components/PopupMsg/popup-manager'
   import { updateTitle } from '@/utils/web-utils'
   import { searchVariants } from '@/utils/chess/variant-search'
+  import type { PublishedVariantGui } from '@/protochess/types'
+  
+  const variantStore = useVariantStore()
+  const authStore = useAuthStore()
+  const playPopup = ref<InstanceType<typeof PlayPopup>>()
+    
+  const showDraftCard = computed(() => {
+    // Do not show draft card if user is not logged in
+    // Also wait until the variant list is fetched to avoid distracting pop-in
+    // This means that if the server still has no variants, the draft card will
+    // not show, but that will never happen in production
+    return authStore.loggedUser && variantStore.variantList.length > 0
+  })
   
   onMounted(() => updateTitle())
+  
+  variantStore.refreshList().catch(e => {
+    console.error(e)
+    showPopup(
+      'Cannot load variants',
+      'There was an error loading the variants. Please try again later.',
+      'ok'
+    )
+  })
+  
+  function playClicked(variant: PublishedVariantGui) {
+    playPopup.value?.show(variant.uid)
+  }
   
   async function search(text: string) {
     console.log(await searchVariants(text))
