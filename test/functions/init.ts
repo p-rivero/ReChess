a59e472d04847a84157856c1ab3dcaee8cec6c32
privero <65060696+p-rivero@users.noreply.github.com>
2023-05-22 08:14:33 +0200

    Add sonarcloud to CI workflow

--- test/functions/init.ts
+++ test/functions/init.ts
@@ -6,13 +6,16 @@ import { FIREBASE_CONFIG } from '../../src/firebase/credentials'
 
 const PROJECT_ID = 'functions-test'
 
-dotenv.config({ path: '.config/.env.local' })
+//! IMPORTANT: Do not run the functions emulator, since it will interfere with the test.
+//! You can use: firebase emulators:exec --only firestore,storage,auth 'npm test'
 process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080"
 process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099"
 process.env.FIREBASE_STORAGE_EMULATOR_HOST = "localhost:9199"
 
+dotenv.config({ path: '.config/.env.local' })
 const config = {
   ...FIREBASE_CONFIG,
+  projectId: PROJECT_ID,
   apiKey: process.env.VITE_RECHESS_FIREBASE_API_KEY,
   credential: admin.credential.cert(require('../../.config/admin-key.json')),
 }
